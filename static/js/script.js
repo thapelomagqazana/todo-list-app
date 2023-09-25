cca3cdd94169fba9ca15574c76275cb021b7bfc1
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-09-25 19:53:48 +0200

    Added client-side validation using JavasScript to provide immediate feedback to users before they submit a form

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,16 @@ $(document).ready(function(){
         $(this).find(".task-details").slideToggle();
     });
 });
+
+function validateForm(){
+    var title = document.forms["taskForm"]["title"].value;
+
+    if (title.length < 5){
+        alert("Title must be at least 5 characters long.")
+        return false
+    }
+
+    // Add other validations rules as needed
+
+
+}
--- taskmanager/forms.py
+++ taskmanager/forms.py
@@ -5,3 +5,11 @@ class Taskform(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description']
+
+    
+    # def clean_title(self):
+    #     title = self.cleaned_data.get("title")
+    #     # Add custom validation logic for the title field
+    #     if len(title) < 5:
+    #         raise forms.ValidationError("Title must be at least 5 characters long.")
+    #     return title
